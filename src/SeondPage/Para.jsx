1e0f4decdae97c2d2a4de2d1e72d91de970f16d9
Sangram <balsangram1@gmail.com>
2023-08-12 16:36:23 +0530

    new page

---
+++ src/SeondPage/Para.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+function Para (){
+    return(
+    <>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi qui tempore asperiores reprehenderit sit dolores at est eum sequi nesciunt.</p>
+    </>
+    )
+}
+
+export default Para;
---
+++ src/SeondPage/ol.jsx
@@ -0,0 +1,17 @@
+import react from "react";
+
+function Ol (){
+    return(
+        <>
+        <h1>gg</h1>
+        <ol>
+            <li>hello</li>
+            <li>guys</li>
+            <li>how</li>
+            <li>are</li>
+            <li>you</li>
+        </ol>
+        </>
+    )
+}
+export default Ol ;
